c59d1f5a8b51914617b27b57e992aa877499cbe3
MrSeaWave <MrDaemon@outlook.com>
2022-02-09 15:50:14 +0800

    feat: update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('11');
+// index.js
+console.log(process.env.npm_package_name); // xxx
+console.log(process.env.npm_package_version); // 0.0.1
+console.log(process.env.npm_config_node_version); // 14.18.1
+console.log(process.env); // env {....}
